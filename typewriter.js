a58ab1075370770e998ac6d2b3726b014d41f57a
Erik Granberg <36399140+erik8036granberg@users.noreply.github.com>
2019-02-11 14:38:44 +0100

    loop working

--- typewriter.js
+++ typewriter.js
@@ -1,14 +1,52 @@
 "use strict";
 
-let conter = 0;
+let text;
+let counter;
 
 window.addEventListener("DOMContentLoaded", init);
 
 function init() {
     console.log("init");
+
+    // save text in variable
+    text = document.querySelector("#typewriter").textContent;
+    console.log(text);
+
+    // Clear text from the div
+    document.querySelector("#typewriter").textContent = "";
+
+    // reset counter
+    counter = 0;
+
+    // start loop
     typeLoop();
 }
 
-function typeLoop() {
 
+function typeLoop() {
+    console.log("loop");
+
+    let tempText;
+    let textLength = text.length;
+    console.log(textLength);
+
+    // find text content from 0 to counter
+    tempText = text.slice(0, counter);
+    console.log(tempText);
+
+    // display text in div
+    document.querySelector("#typewriter").textContent = tempText;
+
+    // counter ++
+    counter++;
+    console.log(counter);
+
+    // delay (400ms)
+
+    // if text.length < counter .....keep looping
+    if (textLength < counter) {
+        typeLoop();
+    } else {
+        alert("done");
+    }
 }
