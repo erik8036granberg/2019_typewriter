883485304fcd001448e9cbc5c2d33e58cbd1bbc3
Erik Granberg <36399140+erik8036granberg@users.noreply.github.com>
2019-02-11 15:02:07 +0100

    loop with timer ok

--- typewriter.js
+++ typewriter.js
@@ -2,51 +2,51 @@
 
 let text;
 let counter;
+let textLength;
 
 window.addEventListener("DOMContentLoaded", init);
 
 function init() {
-    console.log("init");
+  console.log("init");
 
-    // save text in variable
-    text = document.querySelector("#typewriter").textContent;
-    console.log(text);
+  // save text in variable
+  text = document.querySelector("#typewriter").textContent;
+  console.log(text);
 
-    // Clear text from the div
-    document.querySelector("#typewriter").textContent = "";
+  // Clear text from the div
+  document.querySelector("#typewriter").textContent = "";
 
-    // reset counter
-    counter = 0;
+  // reset counter
+  counter = 0;
 
-    // start loop
-    typeLoop();
+  // check length of text
+  textLength = text.length;
+
+  // start loop
+  typeLoop();
 }
 
-
 function typeLoop() {
-    console.log("loop");
+  console.log("loop");
 
-    let tempText;
-    let textLength = text.length;
-    console.log(textLength);
+  let tempText;
 
-    // find text content from 0 to counter
-    tempText = text.slice(0, counter);
-    console.log(tempText);
+  // find text content from 0 to counter
+  tempText = text.slice(0, counter);
+  console.log(tempText);
 
-    // display text in div
-    document.querySelector("#typewriter").textContent = tempText;
+  // display text in div
+  document.querySelector("#typewriter").textContent = tempText;
 
-    // counter ++
-    counter++;
-    console.log(counter);
+  // counter ++
+  counter++;
+  console.log(counter);
 
+  // if text.length < counter .....keep looping
+  if (counter <= textLength) {
     // delay (400ms)
-
-    // if text.length < counter .....keep looping
-    if (textLength < counter) {
-        typeLoop();
-    } else {
-        alert("done");
-    }
+    setTimeout(function() {
+      typeLoop();
+    }, 400);
+  }
 }
