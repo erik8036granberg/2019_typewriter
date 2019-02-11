fc2308888efe37ca7517e57f9b42a8f4460a2e40
Erik Granberg <36399140+erik8036granberg@users.noreply.github.com>
2019-02-11 13:43:33 +0100

    Start js-stuff

--- typewriter.js
+++ typewriter.js
@@ -1,1 +1,14 @@
 "use strict";
+
+let conter = 0;
+
+window.addEventListener("DOMContentLoaded", init);
+
+function init() {
+    console.log("init");
+    typeLoop();
+}
+
+function typeLoop() {
+
+}
